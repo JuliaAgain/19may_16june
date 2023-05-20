6b0f0aed1336a529417794ca939eb5dfeb9d7a5e
Julia <>
2023-05-20 16:55:26 +0300

    Added HomeTask04

---
+++ HomeTask04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+double num3 = Convert.ToDouble(Console.ReadLine());
+double maximum = num1;
+if (num2 > maximum)
+{
+    maximum = num2;
+}
+if (num3 > maximum)
+{
+    maximum = num3;
+}
+Console.WriteLine($"Максимальное из этих трех чисел - {maximum}");
