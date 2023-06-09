45cb7b98cf8a051fd41dc5672759dac506ba99fb
Julia <>
2023-06-09 10:56:55 +0300

    Changed Task48

--- Task48/Program.cs
+++ Task48/Program.cs
@@ -5,3 +5,31 @@
 // 0 1 2 3
 // 1 2 3 4
 // 2 3 4 5
+
+int[,]  CreateMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrix(3, 4);
+PrintMatrix(array2d);
