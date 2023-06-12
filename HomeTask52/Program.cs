205f981312d9489cab51ab051dbf1e69374cfec1
Julia <>
2023-06-12 20:08:28 +0300

    Added Task50

---
+++ HomeTask52/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,]  CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void MeanValueInColumn(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        var mean = (double) sum / matrix.GetLength(0);
+        double meanRound = Math.Round(mean, 1, MidpointRounding.ToZero);
+        Console.Write(meanRound + "; ");
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 5, 0, 10);
+PrintMatrix(array2d);
+Console.WriteLine("Средние арифметические значения для каждого столбца:");
+MeanValueInColumn(array2d);
