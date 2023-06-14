1ffca69c710731cd1a86cd6cce798d835ad77b01
Julia <>
2023-06-14 15:52:33 +0300

    Added Task38

---
+++ HomeTask38/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] CreateArrayRndDouble(int size, int min, int max, int round = 2)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, round);
+    }
+
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr, string sep = ";")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+double DifferenceBetweenMinMax(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    double dif = max - min;
+    double difRound = Math.Round(dif, 2, MidpointRounding.ToZero);
+    return difRound;
+}
+
+double[] row = CreateArrayRndDouble(8, -5, 30, 2);
+PrintArrayDouble(row);
+double difference = DifferenceBetweenMinMax(row);
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным значениями этого массива: {difference}.");
