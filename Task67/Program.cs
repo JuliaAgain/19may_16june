ba0aebaee3db7f8fe6063688aeab4db2f8e26f0a
Julia <>
2023-06-16 11:35:09 +0300

    Added task69

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -8,7 +8,7 @@
 int SumDigits(int n)
  {
      if (n == 0) return 0;
-     else return n % 10 + SumDigits(n / 10);
+     return n % 10 + SumDigits(n / 10);
  }
 
 Console.WriteLine("Введите натуральное число");
