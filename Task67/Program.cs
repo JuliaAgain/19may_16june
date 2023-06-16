3cb356dc605771b9df375d4cd0956a664803aeff
Julia <>
2023-06-16 11:11:34 +0300

    Added Tasks 63, 65, 67

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет 
+// принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+int SumDigits(int n)
+ {
+     if (n == 0) return 0;
+     else return n % 10 + SumDigits(n / 10);
+ }
+
+Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigits(num)); 
